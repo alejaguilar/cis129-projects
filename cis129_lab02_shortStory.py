b5b0c5d4d952729fd4d6b78b565ddc329d63a80b
alejaguilar <aaguilar114@mail.pima.edu>
2025-02-05 18:09:27 -0700

    Updates with story cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author: Alejandra Aguilar
+# My first Python lab in my CIS129 course
+# Prints a very short story
+print("When my puppy saw herself in a mirror for the first time, she got scared.")
+print("She barked and growled at her reflection in an effort to defend against an intruder.")
+print("When she lunged at her image, she spooked herself badly enough to run away whimpering.")
+print("She was a silly puppy.")
